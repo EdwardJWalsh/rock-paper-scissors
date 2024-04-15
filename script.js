00b8863d37c583e8cea18f006044b0fc2c45bef6
EdwardJWalsh <155949281+EdwardJWalsh@users.noreply.github.com>
2024-04-15 12:34:25 +0100

    add javascript to create functionality

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 const buttons = document.querySelectorAll('button');
 
-buttons.forEach(button => {
+buttons.forEach((button) => {
 button.addEventListener('click',() =>{
-    console.log('You clicked me!',button.id);
+    const result=
+    playGame (button.id, computerPlay());
+    console.log(result);
 });
 });
+
+function computerPlay() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomChoice = Math.floor(Math.random() * choices.length);
+    return choices[randomChoice];
+}
+
+function playGame(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === "rock" && 
+        computerSelection === "scissors") ||
+        (playerSelection === "paper" && 
+        computerSelection === "rock") ||
+        (playerSelection === "scissors" && 
+        computerSelection === "paper")
+    ) {return "You win! " + playerSelection + " beats " + computerSelection;}
+      else {
+        return "You lose! " + computerSelection + " beats " + playerSelection;
+    }
+
+
+}
